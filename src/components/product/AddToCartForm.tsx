8b91da27be1371aed3e2ff11589f6a2ba0c17c23
zehael <evg29227704@gmail.com>
2022-11-22 13:28:41 +0500

    fix(cart): fixed logic of add to cart item

--- src/components/product/AddToCartForm.tsx
+++ src/components/product/AddToCartForm.tsx
@@ -1,35 +1,52 @@
-import React, { FC } from 'react';
-import { Button, Space } from 'antd';
-import { addCartItem, CartItem, isCartOpen } from '../../store/cartStore';
-import GoBackButton from '../common/GoBackButton';
-import type { IProduct } from '../../types/product';
+import React, { FC } from "react";
+import { Button, Space } from "antd";
+import {
+  addCartItem,
+  CartItem,
+  cartItems,
+  isCartOpen,
+} from "../../store/cartStore";
+import GoBackButton from "../common/GoBackButton";
+import type { IProduct } from "../../types/product";
+import { useStore } from "@nanostores/react";
 
 interface AddToCartFormProps {
   product: IProduct;
 }
 
 const AddToCartForm: FC<AddToCartFormProps> = ({ product }) => {
+  const $cartItems = useStore(cartItems);
 
   function addToCart(e: React.MouseEvent) {
     e.preventDefault();
     isCartOpen.set(true);
-    const cartItem = createCartItemByProduct();
-    addCartItem(cartItem);
+    const isExists = checkProductInCart();
+    if (!isExists) {
+      const cartItem = createCartItemByProduct();
+      addCartItem(cartItem);
+    }
   }
 
   function createCartItemByProduct(): CartItem {
     return {
       id: product.id,
-      name: product.title,
-      imageSrc: product.image,
-      quantity: 1
-    }
+      name: product.attributes.title,
+      imageSrc: product.attributes.image.data?.attributes?.url || "",
+      quantity: 1,
+    };
+  }
+
+  function checkProductInCart(): boolean {
+    const item = $cartItems.find((item) => item.id === product.id);
+    return item !== undefined;
   }
 
   return (
-    <Space size='middle' direction='vertical' align='end'>
+    <Space size="middle" direction="vertical" align="end">
       <GoBackButton />
-      <Button size='large' type='primary' onClick={addToCart}>Add to cart</Button>
+      <Button size="large" type="primary" onClick={addToCart}>
+        Add to cart
+      </Button>
     </Space>
   );
 };
